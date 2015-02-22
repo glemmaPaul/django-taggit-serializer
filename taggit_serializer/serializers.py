1108d845e87cbeaee65dae2400d9032c1c1f0102
Paul Oostenrijk <paul@glemma.nl>
2015-02-22 13:36:05 -0500

    feat(serializers): Refactored SerializerField to use the ListSerializerField, added TaggitSerializer to the serializers file

--- taggit_serializer/serializers.py
+++ taggit_serializer/serializers.py
@@ -1,17 +1,44 @@
 # -*- coding: utf-8 -*-
-
 from rest_framework import serializers
-from rest_framework.exceptions import ParseError
 
 
-class TagListSerializerField(serializers.Field):
-
-    def to_internal_value(self, data):
-        if type(data) is not list:
-            raise ParseError("expected a list of data")
-        return data
+class TagListSerializerField(serializers.ListField):
 
     def to_representation(self, obj):
         if type(obj) is not list:
             return [tag.name for tag in obj.all()]
         return obj
+
+
+class TaggitSerializer(serializers.Serializer):
+    def create(self, validated_data):
+        to_be_tagged, validated_data = self._pop_tags(validated_data)
+
+        tag_object = super(TaggitSerializer, self).create(validated_data)
+
+        return self._save_tags(tag_object, to_be_tagged)
+
+    def update(self, object, validated_data):
+        to_be_tagged, validated_data = self._pop_tags(validated_data)
+
+        tag_object = super(TaggitSerializer, self).update(
+            object, validated_data)
+
+        return self._save_tags(tag_object, to_be_tagged)
+
+    def _save_tags(self, tag_object, tags):
+        for key in tags.keys():
+            tag_values = tags.get(key)
+            [getattr(tag_object, key).add(tag) for tag in tag_values]
+
+        return tag_object
+
+    def _pop_tags(self, validated_data):
+        to_be_tagged = {}
+
+        for key in self.fields.keys():
+            field = self.fields[key]
+            if isinstance(field, TagListSerializerField):
+                to_be_tagged[key] = validated_data.pop(key)
+
+        return (to_be_tagged, validated_data)
