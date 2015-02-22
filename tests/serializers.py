4e9401e4a83124ba6b8e24e76c4861232650be12
Paul Oostenrijk <paul@glemma.nl>
2015-02-22 13:37:49 -0500

    feat(tests/serializers): Added a test serializer that we use to test our serializer

---
+++ tests/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import TestModel
+from taggit_serializer.serializers import (TagListSerializerField,
+                                           TaggitSerializer)
+
+
+class TestModelSerializer(TaggitSerializer, serializers.ModelSerializer):
+
+    tags = TagListSerializerField()
+
+    class Meta:
+        model = TestModel
